cad26b6465136bf30467138974e0bdd79dd45c31
PDM-pavlyukdm <pavlyukdm@gmail.com>
2020-04-19 15:19:34 +0300

    2
    added swiper
    indentation to standart

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 import $ from '../local_modules/jquery/dist/jquery.min'
+import Swiper from '../local_modules/swiper/js/swiper.esm.bundle'
 
 $(document).ready(() => {
     // eslint-disable-next-line no-console
-    console.log(`document ready`)
+    console.log('document ready')
 })
+
+const mySwiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+})
+mySwiper()
